ff2647d36d3ef8ece76a308ce2574744b7ed6af3
Rei Matsuda <rei2219.matsuda@gmail.com>
2022-06-10 15:26:40 +0900

    typo

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ import './style.scss'
 
 document.querySelector('#app').innerHTML = `
   <h1>media query range-form test</h1>
-  <p>800px < width <= 100px : RED</p>
+  <p>800px < width <= 1000px : RED</p>
 `
